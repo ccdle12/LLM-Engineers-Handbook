47e741302f8c799a0ad3a8933e57cc9ba379a504
ccdle12 <chris.coverdale24@gmail.com>
2025-07-08 17:01:55 +0000

    flake

--- flake.nix
+++ flake.nix
@@ -16,20 +16,33 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        myapp = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
-          projectDir = self;
-          overrides = poetry2nix.overrides.withDefaults (final: super:
-            lib.mapAttrs
-              (attr: systems: super.${attr}.overridePythonAttrs
-                (old: {
-                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
-                }))
-              {
+        # myapp = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
+          # projectDir = self;
+          # overrides = poetry2nix.overrides.withDefaults (final: super:
+            # lib.mapAttrs
+              # (attr: systems: super.${attr}.overridePythonAttrs
+                # (old: {
+                  # nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
+                # }))
+              # {
                 # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
                 # package = [ "setuptools" ];
-              }
-          );
+              # }
+          # );
+        # };
+        myapp = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
+          projectDir = self;
+          overrides = poetry2nix.overrides.withDefaults (final: super: {
+            chromedriver-autoinstaller = super.chromedriver-autoinstaller.overridePythonAttrs (old: {
+              nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ final.setuptools ];
+            });
+
+            jiter = super.jiter.overridePythonAttrs (old: {
+                nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ final.maturin ];
+            });
+          });
         };
+
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
